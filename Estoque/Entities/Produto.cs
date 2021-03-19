16729e9999db43044c3dfe3487cecfc393ca65cc
Gustavo653 <gustavohs2004@gmail.com>
2021-03-19 13:52:29 -0300

    Classe produto criada

---
+++ Estoque/Entities/Produto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Estoque.Entities
+{
+    class Produto
+    {
+        public string Nome { get; set; }
+        public int Quantidade { get; set; }
+        public double Preco { get; set; }
+
+        public Produto(string nome, int quantidade, double preco)
+        {
+            Nome = nome;
+            Quantidade = quantidade;
+            Preco = preco;
+        }
+
+        public double Total()
+        {
+            return Preco * Quantidade;
+        }
+
+        public override string ToString()
+        {
+            return "Produto: " + Nome + ", quantidade: " + Quantidade + ", valor unitário: " + Preco + ", valor total: " + Total();
+        }
+    }
+}
